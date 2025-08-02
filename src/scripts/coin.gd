0027d9abec55b3702acb748b1a5464bbd533df9e
Jaime Crespo <jynus@users.noreply.github.com>
2025-08-02 16:25:54 +0200

    Coin rotation and blinking

--- src/scripts/coin.gd
+++ src/scripts/coin.gd
@@ -13,4 +13,6 @@ func _on_timer_visible_timeout() -> void:
 	show()
 
 func _on_timer_despawn_timeout() -> void:
+	%AnimationPlayer.play("about_to_dissapear")
+	await %AnimationPlayer.animation_finished
 	queue_free()
