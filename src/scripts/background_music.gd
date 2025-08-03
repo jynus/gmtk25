8eb7ca3bbaad73f7abbd02448a6b1c9686a5b7fa
Jaime Crespo <jynus@users.noreply.github.com>
2025-08-03 14:16:29 +0200

    Preload all music
    Also make ghost have a small feedback

--- src/scripts/background_music.gd
+++ src/scripts/background_music.gd
@@ -7,13 +7,13 @@ var fadeout_tween : Tween
 var fadein_tween : Tween
 
 var playlist : Dictionary = {
-	"menu": "res://assets/music/ES_Albatross - Lexica.ogg",
-	"pause": "res://assets/music/ES_Mango Fizz - Jobii.ogg",
-	"game": "res://assets/music/ES_Nintendo Revolution - Rolla Coasta.ogg",
-	"level_complete": "res://assets/music/ES_Fair N Square - William Benckert.ogg",
-	"game_over": "res://assets/music/ES_Ctrl Alt Defeat - Deskant.ogg",
-	"shop": "res://assets/music/ES_Before Chill - Yomoti.ogg",
-	"win": "res://assets/music/ES_Goodbye Goodnight - One Two Feet.ogg",
+	"menu": preload("res://assets/music/ES_Albatross - Lexica.ogg"),
+	"pause": preload("res://assets/music/ES_Mango Fizz - Jobii.ogg"),
+	"game": preload("res://assets/music/ES_Nintendo Revolution - Rolla Coasta.ogg"),
+	"level_complete": preload("res://assets/music/ES_Fair N Square - William Benckert.ogg"),
+	"game_over": preload("res://assets/music/ES_Ctrl Alt Defeat - Deskant.ogg"),
+	"shop": preload("res://assets/music/ES_Before Chill - Yomoti.ogg"),
+	"win": preload("res://assets/music/ES_Goodbye Goodnight - One Two Feet.ogg"),
 }
 var _current_song : String = "menu"
 var playing : bool = false
@@ -26,7 +26,7 @@ var playing : bool = false
 
 func _set_current_song(value):
 	_current_song = value
-	music_player.stream = load(playlist[current_song])
+	music_player.stream = playlist[current_song]
 
 func _ready():
 	music_player.volume_db = DEFAULT_MUSIC_VOLUME
